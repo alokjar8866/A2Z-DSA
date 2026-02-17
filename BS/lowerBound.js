58694ada192166d10b80649e96e0a1b9b104343a
Alok Yadav <alokjar212@gmail.com>
2026-02-17 16:21:20 +0530

    lower bound problem

---
+++ BS/lowerBound.js
@@ -0,0 +1,22 @@
+var lowerBound = function (nums, target) {
+    let low = 0;
+    let high = nums.length - 1;
+     let result = -1;
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        if (nums[mid] == target) {
+            result = mid;
+            high = mid - 1;
+        }
+        else if (target < nums[mid]) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+
+    return result;
+}
+
+console.log(lowerBound([1,2,5,5,5,6,8,9],5));
